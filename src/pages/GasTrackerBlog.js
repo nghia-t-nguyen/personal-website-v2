eaadd52dd61b4353a3f73d6da3ca269c940e50e4
Nghia Nguyen <nguyenn2001@gmail.com>
2025-10-19 02:17:09 -0400

    Update GasTrackerBlog.js

--- src/pages/GasTrackerBlog.js
+++ src/pages/GasTrackerBlog.js
@@ -18,7 +18,7 @@ export default function GasTrackerBlog() {
                 Gas is the fee required to successfully conduct a transaction or execute a contract on a cryptocurrency network.
                 While many websites display gas values, they often focus on a single cryptocurrency, leaving some chains without a dedicated resource.
                 Additionally, these sites typically don't present exchange values in one place, making it difficult for users to compare prices and make informed decisions.
-                My application, <em>Gas Tracker</em>, addresses these issues by consolidating gas values for multiple cryptocurrencies in one location, providing users a comprehensive and easy-to-use tool to stay at the top of their crypto game.
+                My application, <a href='https://gas-trkr.netlify.app/'>Gas Tracker</a>, addresses these issues by consolidating gas values for multiple cryptocurrencies in one location, providing users a comprehensive and easy-to-use tool to stay at the top of their crypto game.
             </p>
             <h2>The Design</h2>
             <h3>Visual Identity</h3>
